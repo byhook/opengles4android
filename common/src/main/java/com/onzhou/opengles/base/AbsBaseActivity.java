1efebac5e91876dbe18b10ca806feff3e5cb8ea6
Andy <byhook@163.com>
2018-11-02 09:55:50 +0800

    统一组件&新建shader组件

---
+++ common/src/main/java/com/onzhou/opengles/base/AbsBaseActivity.java
@@ -0,0 +1,11 @@
+package com.onzhou.opengles.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2018-11-02
+ * @description:
+ */
+public abstract class AbsBaseActivity extends AppCompatActivity {
+}
---
+++ opengles-shader/src/main/java/com/onzhou/opengles/shader/ShaderActivity.java
@@ -0,0 +1,20 @@
+package com.onzhou.opengles.shader;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2018-11-02
+ * @description:
+ */
+public class ShaderActivity extends AbsBaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
