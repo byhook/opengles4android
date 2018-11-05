ace5994defdb8c874005f8e20c83c815395b28db
Andy <byhook@163.com>
2018-11-05 16:57:39 +0800

    归一化坐标

--- opengles-simple/src/main/java/com/onzhou/opengles/simple/RectangleRenderer.java
+++ opengles-simple/src/main/java/com/onzhou/opengles/simple/RectangleRenderer.java
@@ -2,6 +2,7 @@ package com.onzhou.opengles.simple;
 
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.onzhou.opengles.utils.ShaderUtils;
 
@@ -33,14 +34,17 @@ public class RectangleRenderer implements GLSurfaceView.Renderer {
             0.5f, 0.5f, 0.0f
     };
 
+    private final float[] mMatrix = new float[16];
+
     /**
      * 顶点着色器
      */
     private String vertextShader =
             "#version 300 es \n" +
                     "layout (location = 0) in vec4 vPosition;\n"
+                    + "uniform mat4 u_Matrix;\n"
                     + "void main() { \n"
-                    + "gl_Position  = vPosition;\n"
+                    + "gl_Position  = u_Matrix * vPosition;\n"
                     + "gl_PointSize = 10.0;\n"
                     + "}\n";
 
@@ -51,6 +55,8 @@ public class RectangleRenderer implements GLSurfaceView.Renderer {
                     + "void main() { \n"
                     + "fragColor = vec4(0.8,0.5,0.5,1.0); \n"
                     + "}\n";
+
+    private int uMatrixLocation;
 
     public RectangleRenderer() {
         //分配内存空间,每个浮点型占4字节空间
@@ -74,16 +80,32 @@ public class RectangleRenderer implements GLSurfaceView.Renderer {
         mProgram = ShaderUtils.linkProgram(vertexShaderId, fragmentShaderId);
         //在OpenGLES环境中使用程序片段
         GLES30.glUseProgram(mProgram);
+
+        uMatrixLocation = GLES30.glGetUniformLocation(mProgram, "u_Matrix");
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES30.glViewport(0, 0, width, height);
+
+        final float aspectRatio = width > height ?
+                (float) width / (float) height :
+                (float) height / (float) width;
+        if (width > height) {
+            //横屏
+            Matrix.orthoM(mMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            //竖屏
+            Matrix.orthoM(mMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
+
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+
+        GLES30.glUniformMatrix4fv(uMatrixLocation, 1, false, mMatrix, 0);
 
         //准备坐标数据
         GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 0, vertexBuffer);
