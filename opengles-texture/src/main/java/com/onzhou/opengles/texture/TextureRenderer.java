e603138be3ac733d8339a86df13bf397bfb0ed36
byhook <byhook@163.com>
2018-11-10 18:46:52 +0800

    添加注释说明

--- opengles-texture/src/main/java/com/onzhou/opengles/texture/TextureRenderer.java
+++ opengles-texture/src/main/java/com/onzhou/opengles/texture/TextureRenderer.java
@@ -42,34 +42,36 @@ public class TextureRenderer implements GLSurfaceView.Renderer {
 
     /**
      * 顶点坐标
+     * (x,y,z)
      */
     private float[] POSITION_VERTEX = new float[]{
-            0f, 0f, 0f,
-            1f, 1f, 0f,
-            -1f, 1f, 0f,
-            -1f, -1f, 0f,
-            1f, -1f, 0f
+            0f, 0f, 0f,     //顶点坐标V0
+            1f, 1f, 0f,     //顶点坐标V1
+            -1f, 1f, 0f,    //顶点坐标V2
+            -1f, -1f, 0f,   //顶点坐标V3
+            1f, -1f, 0f     //顶点坐标V4
     };
 
     /**
      * 纹理坐标
+     * (s,t)
      */
     private static final float[] TEX_VERTEX = {
-            0.5f, 0.5f,
-            1f, 0f,
-            0f, 0f,
-            0f, 1.0f,
-            1f, 1.0f
+            0.5f, 0.5f, //纹理坐标V0
+            1f, 0f,     //纹理坐标V1
+            0f, 0f,     //纹理坐标V2
+            0f, 1.0f,   //纹理坐标V3
+            1f, 1.0f    //纹理坐标V4
     };
 
     /**
      * 索引
      */
     private static final short[] VERTEX_INDEX = {
-            0, 1, 2,
-            0, 2, 3,
-            0, 3, 4,
-            0, 4, 1
+            0, 1, 2,  //V0,V1,V2 三个顶点组成一个三角形
+            0, 2, 3,  //V0,V2,V3 三个顶点组成一个三角形
+            0, 3, 4,  //V0,V3,V4 三个顶点组成一个三角形
+            0, 4, 1   //V0,V4,V1 三个顶点组成一个三角形
     };
 
     private int uMatrixLocation;
