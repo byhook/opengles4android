d4754995d428c8029003b11d2c135976b0dec8fe
byhook <byhook@163.com>
2018-11-10 21:31:30 +0800

    添加相机模块

---
+++ opengles-camera/src/main/java/com/onzhou/opengles/main/CameraSurfaceActivity.java
@@ -0,0 +1,16 @@
+package com.onzhou.opengles.main;
+
+import android.opengl.GLSurfaceView;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+import com.onzhou.opengles.base.AbsGLSurfaceActivity;
+
+/**
+ * @anchor: andy
+ * @date: 18-11-10
+ */
+
+public class CameraSurfaceActivity extends AbsBaseActivity {
+
+
+}
