f20da83f5b2772a55358c742a34ba98d3b4508b0
byhook <byhook@163.com>
2018-11-10 12:05:30 +0800

    添加纹理模块

---
+++ opengles-texture/src/main/java/com/onzhou/opengles/texture/TextureActivity.java
@@ -0,0 +1,19 @@
+package com.onzhou.opengles.texture;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+
+/**
+ * @anchor: andy
+ * @date: 18-11-10
+ */
+
+public class TextureActivity extends AbsBaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
