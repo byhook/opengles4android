b5c5c339dadfeeab265407dc0a44f3e673e4496d
byhook <byhook@163.com>
2018-11-13 21:53:55 +0800

    优化异常处理

--- opengles-native/src/main/cpp/native_window.cpp
+++ opengles-native/src/main/cpp/native_window.cpp
@@ -50,6 +50,13 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     return JNI_VERSION_1_6;
 }
 
+void ThrowException(JNIEnv *env, const char *exception, const char *message) {
+    jclass clazz = env->FindClass(exception);
+    if (NULL != clazz) {
+        env->ThrowNew(clazz, message);
+    }
+}
+
 void drawColor(JNIEnv *env, jobject obj, jobject surface, jint colorARGB) {
     //分离ARGB
     int alpha = (colorARGB >> 24) & 0xFF;
@@ -61,12 +68,29 @@ void drawColor(JNIEnv *env, jobject obj, jobject surface, jint colorARGB) {
 
     //获取目标surface
     ANativeWindow *window = ANativeWindow_fromSurface(env, surface);
+    if (NULL == window) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to get native window");
+        return;
+    }
     //默认的是RGB_565
-    ANativeWindow_setBuffersGeometry(window, 640, 640, WINDOW_FORMAT_RGBA_8888);
+    int32_t result = ANativeWindow_setBuffersGeometry(window, 640, 640, WINDOW_FORMAT_RGBA_8888);
+    if (result < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to set buffers geometry");
+        //释放窗口
+        ANativeWindow_release(window);
+        window = NULL;
+        return;
+    }
     ANativeWindow_acquire(window);
 
     ANativeWindow_Buffer buffer;
-    ANativeWindow_lock(window, &buffer, NULL);
+    if (ANativeWindow_lock(window, &buffer, NULL) < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to lock native window");
+        //释放窗口
+        ANativeWindow_release(window);
+        window = NULL;
+        return;
+    }
 
     uint32_t *line = (uint32_t *) buffer.bits;
     for (int y = 0; y < buffer.height; y++) {
@@ -76,30 +100,61 @@ void drawColor(JNIEnv *env, jobject obj, jobject surface, jint colorARGB) {
         line = line + buffer.stride;
     }
 
-    ANativeWindow_unlockAndPost(window);
+    if (ANativeWindow_unlockAndPost(window) < 0) {
+        ThrowException(env, "java/lang/RuntimeException",
+                       "unable to unlock and post to native window");
+    }
     //释放窗口
     ANativeWindow_release(window);
+
 }
 
 void drawBitmap(JNIEnv *env, jobject obj, jobject surface, jobject bitmap) {
     //获取bitmap的信息,比如宽和高
     AndroidBitmapInfo info;
-    AndroidBitmap_getInfo(env, bitmap, &info);
+    if (AndroidBitmap_getInfo(env, bitmap, &info) < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to get bitmap info");
+        return;
+    }
 
     char *data = NULL;
     //获取bitmap对应的native指针
-    AndroidBitmap_lockPixels(env, bitmap, (void **) &data);
-    AndroidBitmap_unlockPixels(env, bitmap);
+    if (AndroidBitmap_lockPixels(env, bitmap, (void **) &data) < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to lock pixels");
+        return;
+    }
+    if (AndroidBitmap_unlockPixels(env, bitmap) < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to unlock pixels");
+        return;
+    }
 
     //获取目标surface
     ANativeWindow *window = ANativeWindow_fromSurface(env, surface);
+    if (NULL == window) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to get native window");
+        return;
+    }
     //这里设置为RGBA的方式,总共是4字节32位
-    ANativeWindow_setBuffersGeometry(window, info.width, info.height, WINDOW_FORMAT_RGBA_8888);
+    int32_t result = ANativeWindow_setBuffersGeometry(window, info.width, info.height,
+                                                      WINDOW_FORMAT_RGBA_8888);
+    if (result < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to set buffers geometry");
+        //释放窗口
+        ANativeWindow_release(window);
+        window = NULL;
+        return;
+    }
     ANativeWindow_acquire(window);
 
     ANativeWindow_Buffer buffer;
     //锁定窗口的绘图表面
-    ANativeWindow_lock(window, &buffer, NULL);
+    if (ANativeWindow_lock(window, &buffer, NULL) < 0) {
+        ThrowException(env, "java/lang/RuntimeException", "unable to lock native window");
+        //释放窗口
+        ANativeWindow_release(window);
+        window = NULL;
+        return;
+    }
 
     //转换为像素点来处理
     int32_t *bitmapPixes = (int32_t *) data;
@@ -111,7 +166,10 @@ void drawBitmap(JNIEnv *env, jobject obj, jobject surface, jobject bitmap) {
         line = line + buffer.stride;
     }
     //解锁窗口的绘图表面
-    ANativeWindow_unlockAndPost(window);
+    if (ANativeWindow_unlockAndPost(window) < 0) {
+        ThrowException(env, "java/lang/RuntimeException",
+                       "unable to unlock and post to native window");
+    }
     //释放
     ANativeWindow_release(window);
 
