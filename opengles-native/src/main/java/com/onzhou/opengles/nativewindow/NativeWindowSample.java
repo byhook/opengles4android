92a77d62bf809bee3f3eb3632a1afdbaf275f2ce
Andy <byhook@163.com>
2018-11-13 13:28:38 +0800

    去掉多余的代码

--- opengles-native/src/main/java/com/onzhou/opengles/nativewindow/NativeWindowSample.java
+++ opengles-native/src/main/java/com/onzhou/opengles/nativewindow/NativeWindowSample.java
@@ -27,24 +27,4 @@ public class NativeWindowSample {
      */
     public native void drawBitmap(Object surface, Object bitmap);
 
-    /*public byte[] bitmap2RGB(Bitmap bitmap) {
-        //返回可用于储存此位图像素的最小字节数
-        int bytes = bitmap.getByteCount();
-        //创建字节缓冲区
-        ByteBuffer buffer = ByteBuffer.allocate(bytes);
-        //将位图的像素复制到指定的缓冲区
-        bitmap.copyPixelsToBuffer(buffer);
-        byte[] rgba = buffer.array();
-        byte[] pixels = new byte[(rgba.length / 4) * 3];
-
-        int count = rgba.length / 4;
-
-        //Bitmap像素点的色彩通道排列顺序是RGBA
-        for (int i = 0; i < count; i++) {
-            pixels[i * 3] = rgba[i * 4];        //R
-            pixels[i * 3 + 1] = rgba[i * 4 + 1];    //G
-            pixels[i * 3 + 2] = rgba[i * 4 + 2];       //B
-        }
-        return pixels;
-    }*/
 }
