05b20217468b3ba57af7d29fcaefec400e3dc842
Andy <byhook@163.com>
2018-11-13 10:03:40 +0800

    添加NativeWindow模块

---
+++ opengles-native/src/main/java/com/onzhou/opengles/main/NativeWindowActivity.java
@@ -0,0 +1,11 @@
+package com.onzhou.opengles.main;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2018-11-13
+ * @description:
+ */
+public class NativeWindowActivity extends AbsBaseActivity {
+}
