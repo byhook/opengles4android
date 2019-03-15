3937532d29ceb9d80fbb46df77769b37474e026c
Andy <byhook@163.com>
2019-03-15 14:42:34 +0800

    初步添加滤镜组件

---
+++ opengles-filter/src/main/java/com/onzhou/opengles/main/FilterActivity.java
@@ -0,0 +1,19 @@
+package com.onzhou.opengles.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2019-03-15
+ * @description:
+ */
+public class FilterActivity extends AbsBaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
