2fb45a9d9d7c7b9cc171dc71c11f4af79e447b45
Andy <byhook@163.com>
2019-03-27 10:52:54 +0800

    新增统一样例组件

---
+++ opengles-sample/src/main/java/com/onzhou/opengles/sample/SampleActivity.java
@@ -0,0 +1,20 @@
+package com.onzhou.opengles.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.onzhou.opengles.base.AbsBaseActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2019-03-27
+ * @description:
+ */
+public class SampleActivity extends AbsBaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opengles_sample);
+    }
+}
